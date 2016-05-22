9f2d2d6cb49572a21d57a65c760e4674ec8ae4f9
李宏吉 <lihongji@meituan.com>
2016-05-22 13:22:37 +0800

    add

--- 1.javascript/5.ecmascript/es6/ecma6/js/module.js
+++
@@ -1,6 +0,0 @@
-//使用export命令定义了模块的对外接口以后，
-// 其他JS文件就可以通过import命令加载这个模块（文件）。
-
-import {firstName, lastName, year} from './export';
-
-console.log(firstName, lastName, year);
--- 1.javascript/5.ecmascript/es6/ecma6/js/unsupport/generator.js
+++
@@ -1,9 +0,0 @@
-function* helloWorldGenerator() {
-    yield 'hello';
-    yield 'world';
-    return 'ending';
-}
-
-var hw = helloWorldGenerator();
-
-
